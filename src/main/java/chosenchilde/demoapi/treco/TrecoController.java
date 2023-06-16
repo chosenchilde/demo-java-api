eae4bd40190985798ef1c19db2b34df0b6911811
Chosen Child <edencyberspace@gmail.com>
2023-06-15 21:41:10 -0300

    put patch

--- src/main/java/chosenchilde/demoapi/treco/TrecoController.java
+++ src/main/java/chosenchilde/demoapi/treco/TrecoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -20,46 +21,94 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class TrecoController {
 
 	@Autowired
-	private TrecoRepository trecoRepository;
+	private TrecoRepository repository;
 
 	@GetMapping
 	public List<Treco> getAll() {
-		return trecoRepository.findAll();
+		// O método "findAll()" do JPA retorna todos os registros em uma lista.
+		return repository.findAll();
 	}
-	
-	@GetMapping(path = "/{id}", produces = "application/json")
-	public String getOne(@PathVariable Long id) {
 
-		if (trecoRepository.existsById(id)) {
-			try {
-				ObjectMapper mapper = new ObjectMapper();
-				Treco treco = trecoRepository.findById(id).get();
-				return mapper.writeValueAsString(treco);
-			} catch (JsonProcessingException e) {
-				e.printStackTrace();
-			}
+	@GetMapping(path = "/{id}", produces = "application/json")
+	public String getOne(@PathVariable Long id) throws JsonProcessingException {
+
+		// Se o registro com o Id existe.
+		if (repository.existsById(id)) {
+
+			// ObjectMapper tenta converter um objeto para JSON.
+			ObjectMapper mapper = new ObjectMapper();
+
+			// Obtém o registro pelo Id e armazena no objeto "treco".
+			Treco treco = repository.findById(id).get();
+
+			// Retorna "treco" convertido para JSON (String → JSON).
+			return mapper.writeValueAsString(treco);
 		}
+
+		// Se o registro não existe, retorna o JSON.
 		return "{ \"status\" : \"not found\" }";
 
 	}
 
 	@PostMapping
 	public Treco post(@RequestBody Treco treco) {
-		return trecoRepository.save(treco);
+
+		// O método "save()" de JPA cria um novo registro
+		// e armazena o objeto nele.
+		return repository.save(treco);
 	}
 
 	@DeleteMapping(path = "/{id}", produces = "application/json")
 	public String delete(@PathVariable Long id) {
-		if (trecoRepository.existsById(id)) {
-			trecoRepository.deleteById(id);
+
+		// Se o registro com o Id existe.
+		if (repository.existsById(id)) {
+
+			// O método "deleteById()" de JPA apaga o registro pelo Id.
+			repository.deleteById(id);
+
+			// Se apagou, retorna o JSON.
 			return "{ \"status\" : \"deleted\" }";
 		}
-		return "{ \"status\" : \"error\" }";
+
+		// Se o registro não existe, retorna o JSON.
+		return "{ \"status\" : \"not found\" }";
 	}
 
-	@PutMapping(path = "/{id}")
-	public Treco put(@PathVariable Long id, @RequestBody Treco treco) {
-		return null;
+	@PutMapping(path = "/{id}", produces = "application/json")
+	public String updateAll(@PathVariable Long id, @RequestBody Treco treco) throws JsonProcessingException {
+
+		// Se o registro com o Id existe.
+		if (repository.existsById(id)) {
+
+			// Obtém o registro pelo Id e mapeia seus atributos para "temp".
+			repository.findById(id).map(temp -> {
+
+				// Atualiza os campos, 1 por 1 conforme os valores fornecidos por "treco".
+				temp.setDate(treco.getDate());
+				temp.setName(treco.getName());
+				temp.setDescription(treco.getDescription());
+				temp.setStatus(treco.getStatus());
+
+				// Salva o registro atualizado.
+				return repository.save(temp);
+
+				// Se Se não consegue obter o registro, ex. Id errado...
+			}).orElseGet(() -> {
+
+				// Não faz nada.
+				return null;
+			});
+
+			// Retorna o registro atualizado usando o método GET.
+			// Nota: adicione "throws JsonProcessingException" ao método "updateAll()".
+			return getOne(id);
+
+		}
+
+		// Se o registro não existe, retorna o JSON.
+		return "{ \"status\" : \"not found\" }";
+
 	}
 
 	@PatchMapping(path = "/{id}")
